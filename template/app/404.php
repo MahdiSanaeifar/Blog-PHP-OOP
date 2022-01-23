45f633de05c35bf1e1d5b11b575c31a46c98435b
MahdiSanaeifar <mahdisanaeifar@gmail.com>
2022-01-23 09:18:30 +0330

    create 404 page front

---
+++ template/app/404.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html class="no-js" lang="fa">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>خطای ۴۰۴</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="https://domain.ir/public/app/img/favicon.png">
+    <!-- Normalize CSS -->
+    <link rel="stylesheet" href="https://domain.ir/public/app/css/normalize.css">
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="https://domain.ir/public/app/css/main.css">
+    <!-- Bootstrap rtl CSS -->
+    <link rel="stylesheet" href="https://domain.ir/public/app/css/bootstrap-rtl.min.css">
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="https://domain.ir/public/app/css/animate.min.css">
+    <!-- Font-awesome CSS-->
+    <link rel="stylesheet" href="https://domain.ir/public/app/css/font-awesome.min.css">
+    <!-- Owl Caousel CSS -->
+    <link rel="stylesheet" href="https://domain.ir/public/app/vendor/OwlCarousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="https://domain.ir/public/app/vendor/OwlCarousel/owl.theme.default.min.css">
+    <!-- Main Menu CSS -->
+    <link rel="stylesheet" href="https://domain.ir/public/app/css/meanmenu.min.css">
+    <!-- Nivo Slider CSS-->
+    <link rel="stylesheet" href="https://domain.ir/public/app/vendor/slider/css/nivo-slider.css"
+          type="text/css">
+    <link rel="stylesheet" href="https://domain.ir/public/app/vendor/slider/css/preview.css" type="text/css"
+          media="screen">
+    <!-- Magnific CSS -->
+    <link rel="stylesheet" type="text/css" href="https://domain.ir/public/app/css/magnific-popup.css">
+    <!-- Switch Style CSS -->
+    <link rel="stylesheet" href="https://domain.ir/public/app/css/hover-min.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="https://domain.ir/public/app/css/style.css">
+    <!-- For IE -->
+    <link rel="stylesheet" type="text/css" href="https://domain.ir/public/app/css/ie-only.css">
+    <!-- Modernizr Js -->
+    <script src="https://domain.ir/public/app/js/modernizr-2.8.3.min.js"></script>
+
+</head>
+
+<body class="bg-primary">
+
+    <!-- 404 Error Page Area Start Here -->
+    <section class="pt-100 pb-100">
+        <div class="container">
+            <div class="text-center">
+                <img src="https://domain.ir/public/app/img/404.png" alt="404" class="img-fluid m-auto" width="300px">
+                <h2 class="title-regular-light size-c60 mb-60">متاسفانه صفحه مورد نظر شما یافت نشد</h2>
+                <a href="https://domain.ir/" class="btn-gtf-ltl-64">رفتن به صفحه خانه</a>
+            </div>
+        </div>
+    </section>
+    <!-- 404 Error Page Area End Here -->
+
+<!-- jquery-->
+<script src="https://domain.ir/public/app/js/jquery-2.2.4.min.js" type="text/javascript"></script>
+<!-- Plugins js -->
+<script src="https://domain.ir/public/app/js/plugins.js" type="text/javascript"></script>
+<!-- Popper js -->
+<script src="https://domain.ir/public/app/js/popper.js" type="text/javascript"></script>
+<!-- Bootstrap js -->
+<script src="https://domain.ir/public/app/js/bootstrap.min.js" type="text/javascript"></script>
+<!-- WOW JS -->
+<script src="https://domain.ir/public/app/js/wow.min.js"></script>
+<!-- Owl Cauosel JS -->
+<script src="https://domain.ir/public/app/vendor/OwlCarousel/owl.carousel.min.js"
+        type="text/javascript"></script>
+<!-- Meanmenu Js -->
+<script src="https://domain.ir/public/app/js/jquery.meanmenu.min.js" type="text/javascript"></script>
+<!-- Srollup js -->
+<script src="https://domain.ir/public/app/js/jquery.scrollUp.min.js" type="text/javascript"></script>
+<!-- jquery.counterup js -->
+<script src="https://domain.ir/public/app/js/jquery.counterup.min.js"></script>
+<script src="https://domain.ir/public/app/js/waypoints.min.js"></script>
+<!-- Nivo slider js -->
+<script src="https://domain.ir/public/app/vendor/slider/js/jquery.nivo.slider.js"
+        type="text/javascript"></script>
+<script src="https://domain.ir/public/app/vendor/slider/home.js" type="text/javascript"></script>
+<!-- Isotope js -->
+<script src="https://domain.ir/public/app/js/isotope.pkgd.min.js" type="text/javascript"></script>
+<!-- Magnific Popup -->
+<script src="https://domain.ir/public/app/js/jquery.magnific-popup.min.js"></script>
+<!-- Ticker Js -->
+<script src="https://domain.ir/public/app/js/ticker.js" type="text/javascript"></script>
+<!-- Custom Js -->
+<script src="https://domain.ir/public/app/js/main.js" type="text/javascript"></script>
+</body>
+
+</html>
